f1d217921493281732d883b2a917785390edf457
dparrott96 <33380605+dparrott96@users.noreply.github.com>
2017-11-29 15:07:06 -0500

    Add files via upload

---
+++ SceneChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public void changemenuscene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
+
